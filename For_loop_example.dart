0ba60dd7bf7e060967e84bfa820e40323e2f9cc0
MuhammadTahaSherazi <muhammadtahasherazi@gmail.com>
2024-01-17 21:05:08 +0500

    for loop task

---
+++ For_loop_example.dart
@@ -0,0 +1,20 @@
+void main() {
+   // List<dynamic> lc_frnd = ["Taha","Ghazanfar","Huzaifa","Bilal","testing123"];
+    
+    List<Map> lc_student = [{"name" : "Ahmed","age" : 20},
+                            {"name" : "Ali","age" : 16},
+                            {"name" : "Akram","age" : 18},
+                           ];
+    var lc_cnt = 0;
+    
+    for (;lc_cnt < lc_student.length;) {
+        if (lc_cnt == lc_student.length-1){
+          print(lc_student[lc_cnt]["age"]);
+        }
+        else
+        {
+          print(lc_student[lc_cnt]);
+        }
+        lc_cnt++;
+    }
+}
---
+++ Quiz01.dart
@@ -0,0 +1,18 @@
+// Void main() {
+// Var lc_usr = "taha";
+// Var lc_pas = "test";
+
+// If lc_usr != "taha" && lc_pas != "test" {
+//   print("Invalid user and Password");
+// }
+// elseif lc_usr != "taha" {
+//   Print("invalid user name");
+// }
+// Elseif lc_pas != "test" {
+//   Print("invalid password");
+// }
+// Else {
+//   print("Login Successfully");
+// }
+
+// };
